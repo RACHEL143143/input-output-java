a597710670b1f42bbfcd8e66c5173bb1d32c1639
RACHEL143143 <rachelrachel1400@gmail.com>
2025-12-12 15:06:14 +0530

    Add files via upload

---
+++ Doubleword.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Doubleword {
+    public static void main(){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        System.out.println('"'+a+'"');
+    }
+}
