110d5ebae4e864b51881e70193b71091024e7492
RACHEL143143 <rachelrachel1400@gmail.com>
2025-12-12 12:36:11 +0530

    Add files via upload

---
+++ fract.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class fract {
+    public static void main(String args []){
+    Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble();
+        System.out.printf("%.2f",a);
+}
+}
