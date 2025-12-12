b021ce4f4952c4d2a638f6cd7f619715ef7990b4
RACHEL143143 <rachelrachel1400@gmail.com>
2025-12-12 14:45:49 +0530

    Add files via upload

---
+++ Toint.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Toint {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        char a=sc.next().charAt(0);
+        int ch=(int)a;
+        System.out.println(ch);
+    }
+}
